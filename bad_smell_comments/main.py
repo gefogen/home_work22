9eaee4a904aad2bb42157964852f463cb08bafd6
GeFoGen <gefogen>
2022-08-29 15:13:37 +0300

    home_work22

---
+++ bad_smell_comments/main.py
@@ -0,0 +1,30 @@
+# В данном коде все прокомментировано как надо,
+# но слишком избыточно.
+# Избавьтесь от комментариев, изменив имена переменных, 
+# чтобы было понятно, за что эти переменные отвечают 
+# и что происходит и без комментариев
+
+
+class Unit:
+    def __init__(self):
+        pass
+
+    def move(self, direction):
+        speed = self._get_speed()
+
+        if direction == 'UP':
+            self.field.set_unit(y=self.y + speed, x=self.x, unit=self)
+        elif direction == 'DOWN':
+            self.field.set_unit(y=self.y - speed, x=self.x, unit=self)
+        elif direction == 'LEFT':
+            self.field.set_unit(y=self.y, x=self.x - speed, unit=self)
+        elif direction == 'RIGTH':
+            self.field.set_unit(y=self.y, x=self.x + speed, unit=self)
+
+    def _get_speed(self):
+        if self.state == 'fly':
+            return self.speed * 1.2
+        elif self.state == 'crawl':
+            return self.speed * 0.5
+        else:
+            raise ValueError('Эк тебя раскорячило')
